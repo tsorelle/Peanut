08702cc19c8c9c336af833be0b36ba58be0c93ff
Terry SoRelle <tls@2quakers.net>
2017-12-19 16:04:35 -0600

    added case parameter

--- pnut.root/modules/pnut/components/translateComponent.js
+++ pnut.root/modules/pnut/components/translateComponent.js
@@ -13,8 +13,24 @@ var Peanut;
             if (!params.translator) {
                 throw ('owner parameter required:  "translator: self"');
             }
+            var textcase = params["case"] ? params["case"] : '';
             var defaultText = params["default"] ? params["default"] : params.code;
             var text = params.translator().translate(params.code, defaultText);
+            var textLength = text.length;
+            if (textLength > 0) {
+                switch (textcase) {
+                    case 'ucfirst':
+                        text = text.substr(0, 1).toLocaleUpperCase() +
+                            (textLength > 1 ? text.substr(1, textLength) : '');
+                        break;
+                    case 'upper':
+                        text = text.toLocaleUpperCase();
+                        break;
+                    case 'lower':
+                        text = text.toLocaleLowerCase();
+                        break;
+                }
+            }
             me.text(text);
         }
         return translateComponent;
--- pnut.root/modules/pnut/components/translateComponent.ts
+++ pnut.root/modules/pnut/components/translateComponent.ts
@@ -14,8 +14,24 @@ namespace Peanut {
             if (!params.translator) {
                 throw('owner parameter required:  "translator: self"')
             }
+            let textcase = params.case ? params.case : '';
             let defaultText =  params.default ? params.default : params.code;
             let text = (<ViewModelBase>params.translator()).translate(params.code,defaultText);
+            let textLength = text.length;
+            if (textLength > 0) {
+                switch (textcase) {
+                    case 'ucfirst' :
+                        text = text.substr(0, 1).toLocaleUpperCase() +
+                            (textLength > 1 ? text.substr(1,textLength) : '');
+                        break;
+                    case 'upper' :
+                        text = text.toLocaleUpperCase();
+                        break;
+                    case 'lower' :
+                        text = text.toLocaleLowerCase();
+                        break;
+                }
+            }
             me.text(text);
         }
     }
